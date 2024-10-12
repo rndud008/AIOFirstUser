a39369568b9b3f4fdaa1c8d26f322f953824e894
rndud008 <rndud008@gamil.com>
2024-10-12 15:58:59 +0900

    user repo commit

---
+++ src/main/java/hello/aiofirstuser/AioFirstUserApplication.java
@@ -0,0 +1,13 @@
+package hello.aiofirstuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioFirstUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AioFirstUserApplication.class, args);
+    }
+
+}
